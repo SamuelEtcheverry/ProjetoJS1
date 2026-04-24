611374425e29693d5f4cb3575cde4f8a56f3b862
Samuel <samuel.e.l.e07@gmail.com>
2026-04-24 09:48:20 -0300

    feat: html semântico

--- src/js/script.js
+++ src/js/script.js
@@ -26,4 +26,13 @@ const precoProduto =199.99;
 const desconto = 0.15;
 const precoFinal = precoProduto * (1 - desconto);
 console.log(precoFinal)
-console.log('R$ ${precoFinal.toFixed(2)}');
+console.log(`R$ ${precoFinal.toFixed(2)}`);
+
+//metodo de array
+const produtos = [
+    {nome:"Teclado Mecânico", preco:200,promocao:true},
+    {nome:"Mouse Gamer", preco:300,promocao:false},
+    {nome:"Monitor Gamer", preco:900,promocao:true},
+    {nome:"Pad Mouse", preco:70,promocao:false},
+]
+console.log(produtos)
